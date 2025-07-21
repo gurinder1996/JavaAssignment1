e0c432ef4bab3b72ad959edcf2176d4d80fbf28b
Gurinder Singh <gurindersingh13@outlook.com>
2025-07-21 08:03:27 -0400

    Complete Exercise 2.25

---
+++ EX225/OddOrEven.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class OddOrEven {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter an integer: ");
+    int number = input.nextInt();
+
+    if (number % 2 == 0) {
+      System.out.printf("%d is an even number.%n", number);
+    }
+
+    if (number % 2 != 0) {
+      System.out.printf("%d is an odd number.%n", number);
+    }
+    
+    input.close();
+  } 
+} 
