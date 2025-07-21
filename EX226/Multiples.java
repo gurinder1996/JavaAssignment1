2f53cb496afa4d51710d7b1068dec98742a42536
Gurinder Singh <gurindersingh13@outlook.com>
2025-07-21 08:05:43 -0400

    Complete Exercise 2.26

---
+++ EX226/Multiples.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Multiples {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter first integer: ");
+    int num1 = input.nextInt();
+
+    System.out.print("Enter second integer: ");
+    int num2 = input.nextInt();
+
+    if (num1 % num2 == 0) {
+      System.out.printf("%d is a multiple of %d.%n", num1, num2);
+    }
+
+    if (num1 % num2 != 0) {
+      System.out.printf("%d is not a multiple of %d.%n", num1, num2);
+    }
+
+    input.close();
+  } 
+} 
