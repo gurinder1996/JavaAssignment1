2bb6f1971c2adc708b1d2fa64741d3588cab5814
Gurinder Singh <gurindersingh13@outlook.com>
2025-07-21 08:14:44 -0400

    Complete Exercise 3.15

---
+++ EX315/AccountTest.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class AccountTest {
+  public static void main(String[] args) {
+    Account account1 = new Account("Jane Green", 50.00);
+    Account account2 = new Account("John Blue", -7.53);
+
+    displayAccount(account1);
+    displayAccount(account2);
+    
+    Scanner input = new Scanner(System.in);
+
+    System.out.printf("%nEnter deposit amount for account1: ");
+    double depositAmount = input.nextDouble();
+    System.out.printf("%nadding %.2f to account1 balance%n%n", depositAmount);
+    account1.deposit(depositAmount);
+
+    displayAccount(account1);
+    displayAccount(account2);
+
+    System.out.printf("%nEnter deposit amount for account2: ");
+    depositAmount = input.nextDouble();
+    System.out.printf("%nadding %.2f to account2 balance%n%n", depositAmount);
+    account2.deposit(depositAmount);
+
+    displayAccount(account1);
+    displayAccount(account2);
+
+    input.close();
+  }
+  
+  public static void displayAccount(Account accountToDisplay) {
+    System.out.printf("%s balance: $%.2f%n", 
+      accountToDisplay.getName(), accountToDisplay.getBalance());
+  }
+}
